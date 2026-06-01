7b014d7d43383fb4915f7fee479cb6e7a00ca096
KR-AIgit <biggmoney0218@gmail.com>
2026-06-01 23:33:19 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -350,11 +350,7 @@ function initCustomDropdown() {
 }
 
 function setupEventListeners() {
-    const stockSelect = document.getElementById("stock-select");
-    stockSelect.addEventListener("change", (e) => {
-        currentStock = e.target.value;
-        loadStockData(currentStock);
-    });
+    // stockSelect removed
 
     const tabLinks = document.querySelectorAll(".tab-link");
     tabLinks.forEach(link => {
@@ -573,13 +569,43 @@ function loadStockData(stockCode) {
     
     const stockName = encodeURIComponent(db.name);
     const isOriginal = !!STOCK_DATABASE[stockCode];
-    
-    if (isOriginal) {
-        if (lnkNews) lnkNews.href = `https://finance.naver.com/item/news.naver?code=${stockCode}`;
-        if (lnkResearch) lnkResearch.href = `https://finance.naver.com/item/coinfo.naver?code=${stockCode}`;
-        if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockCode}`;
-    } else {
-        if (lnkNews) lnkNews.href = `https://search.naver.com/search.naver?where=news&query=${stockName}+주식`;
+      
+      // Remove any previous onclick handlers
+      if (lnkResearch) lnkResearch.onclick = null;
+      if (lnkNews) lnkNews.onclick = null;
+
+      if (isOriginal) {
+          if (lnkNews) lnkNews.href = `https://finance.naver.com/item/news.naver?code=${stockCode}`;
+          if (lnkResearch) lnkResearch.href = `https://finance.naver.com/item/main.naver?code=${stockCode}`; // Link to comprehensive info (Image 4)
+          if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockCode}`;
+      } else {
+          if (lnkNews) lnkNews.href = `https://search.naver.com/search.naver?where=news&query=${stockName}+주식`;
+          
+          if (lnkResearch) {
+              lnkResearch.href = "#";
+              lnkResearch.onclick = (e) => {
+                  e.preventDefault();
+                  // Create a hidden form to submit the query in EUC-KR encoding to Naver Finance Search
+                  const form = document.createElement('form');
+                  form.action = 'https://finance.naver.com/search/searchList.naver';
+                  form.method = 'get';
+                  form.target = '_blank';
+                  form.acceptCharset = 'euc-kr';
+                  
+                  const input = document.createElement('input');
+                  input.type = 'hidden';
+                  input.name = 'query';
+                  input.value = db.name; // e.g., "펄어비스"
+                  
+                  form.appendChild(input);
+                  document.body.appendChild(form);
+                  form.submit();
+                  document.body.removeChild(form);
+              };
+          }
+          
+          if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockName}`;
+      }+주식`;
         if (lnkResearch) lnkResearch.href = `https://m.stock.naver.com/search/custom?keyword=${stockName}`;
         if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockName}`;
     }
