23360bf4c3d0872baa117d0b961e7edec357407f
KR-AIgit <biggmoney0218@gmail.com>
2026-06-01 23:05:05 +0900

    Add files via upload

---
+++ fix_js.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('app.js', 'utf8');
+
+const regex = /commentForm\.addEventListener\("submit", handleCommentSubmit\);/;
+content = content.replace(regex, `if (commentForm) { commentForm.addEventListener("submit", handleCommentSubmit); }`);
+
+fs.writeFileSync('app.js', content, 'utf8');
+console.log('Fixed Type Error');
---
+++ fix_links3.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('app.js', 'utf8');
+
+const oldLinks = "    const stockName = encodeURIComponent(db.name);\n    if (lnkNews) lnkNews.href = `https://search.naver.com/search.naver?where=news&query=${stockName}+주식`;\n    if (lnkResearch) lnkResearch.href = `https://finance.naver.com/search/searchList.naver?query=${stockName}`;\n    if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockName}`;";
+
+const newLinks = "    const stockName = encodeURIComponent(db.name);\n    const isOriginal = !!STOCK_DATABASE[stockCode];\n    \n    if (isOriginal) {\n        if (lnkNews) lnkNews.href = `https://finance.naver.com/item/news.naver?code=${stockCode}`;\n        if (lnkResearch) lnkResearch.href = `https://finance.naver.com/item/coinfo.naver?code=${stockCode}`;\n        if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockCode}`;\n    } else {\n        if (lnkNews) lnkNews.href = `https://search.naver.com/search.naver?where=news&query=${stockName}+주식`;\n        if (lnkResearch) lnkResearch.href = `https://m.stock.naver.com/search/custom?keyword=${stockName}`;\n        if (lnkHankyung) lnkHankyung.href = `https://consensus.hankyung.com/analysis/list?sdate=&edate=&search_text=${stockName}`;\n    }";
+
+if (content.includes(oldLinks)) {
+    content = content.replace(oldLinks, newLinks);
+    fs.writeFileSync('app.js', content, 'utf8');
+    console.log('Update complete.');
+} else {
+    console.log('Error: Could not find old links logic block.');
+}
